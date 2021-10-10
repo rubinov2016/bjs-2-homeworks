9fb3abec4d266f54124f8935e810cc2647ce803e
Ivan Rubinov <lrubinov@yandex.ru>
2021-10-10 19:29:46 +0300

    second commit variables

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,31 +1,59 @@
 // Задание 1
 function getArrayParams(arr) {
-  let min, max, sum, avg;
-
-  // Ваш код
-
-  return { min: min, max: max, avg: avg };
+  let min,max,sum,avg;
+  if (arr.length > 0) {
+    min = arr[0];
+    max = arr[0];
+    sum = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {max = arr[i]};
+      if (arr[i] < min) {min = arr[i]};
+      sum += arr[i];
+    }
+    avg = (sum / arr.length).toFixed(2);
+    avg = +avg;  
+  }
+  return { min:min, max:max, avg:avg };
 }
 
 // Задание 2
 function worker(arr) {
   let sum;
-
-  // Ваш код
-
+  if (arr.length > 0) {
+    sum = 0;
+    for (let i = 0; i < arr.length; i++) {
+      sum += arr[i];
+    }
+  }
   return sum;
 }
 
 function makeWork(arrOfArr, func) {
   let max;
-
-  // Ваш кода
-  // for ...
-  
+  if (arrOfArr.length > 0) {
+    max = func(arrOfArr[0]);    
+    for (let i = 1; i < arrOfArr.length; i++) {      
+      //let currentSum = func(arrOfArr[i]);
+      //if (currentSum > max) {max = currentSum};
+      if (func(arrOfArr[i]) > max) {
+        max = func(arrOfArr[i]);
+      }
+    }
+  }  
   return max;
 }
 
 // Задание 3
 function worker2(arr) {
-  // Ваш код
+  let min;
+  let max;
+  if (arr.length > 0) {
+    min = arr[0];
+    max = arr[0];
+    for (let i = 1; i < arr.length; i++) {
+      if (arr[i] > max) {max = arr[i]};
+      if (arr[i] < min) {min = arr[i]};
+    }
+  }
+  return max-min;
 }
--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,11 @@
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
+  let result; 
+  result = (arr1.length === arr2.length) &&  arr1.every((item, index) => item === arr2[index]);
   return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
+  resultArr = arr.filter(item => item > 0).filter(item => item % 3 === 0).map(item => item * 10)
   return resultArr; // array
 }
